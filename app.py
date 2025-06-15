e7413e1ee417fa8a61df059697f6e053362250db
Adebiyi1 <147954311+Adebiyi1@users.noreply.github.com>
2025-06-15 09:12:39 -1100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from tensorflow.keras.models import load_model
+import tensorflow as tf 
+from tf.keras.models import load_model
 import numpy as np
 
 # Load the trained model
